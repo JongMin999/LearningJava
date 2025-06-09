9ec48885334a466cbd7845d7eed773da17c3189f
MalRang <ekaloses@naver.com>
2025-06-09 15:27:19 +0900

    2025.06.09(월) chapter35 추상화

---
+++ LearningJava/src/exercise/chapter_35/Fish.java
@@ -0,0 +1,20 @@
+package exercise.chapter_35;
+
+public class Fish extends Animal {
+    private boolean havingPoison;
+    private String livingSea;
+
+    @Override
+    public void eat(String food) {
+        System.out.printf("물고기가 %s를(을) 주둥이로 먹습니다.\n", food);
+    }
+
+    @Override
+    public void sleep(){
+        System.out.println("물고기가 잠을 자고 있습니다.");
+    }
+
+    public void swim(){
+        System.out.println("물고기가 수영을 하고 있습니다.");
+    }
+}
