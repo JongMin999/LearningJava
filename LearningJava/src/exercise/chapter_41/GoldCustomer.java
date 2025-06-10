2b37dd4b4ac1f67eaae288119125b2ddd9eff605
MalRang <ekaloses@naver.com>
2025-06-10 09:04:12 +0900

    2025.06.10(화) chapter41 Heap, 런타임 확인

---
+++ LearningJava/src/exercise/chapter_41/GoldCustomer.java
@@ -0,0 +1,24 @@
+package exercise.chapter_41;
+
+public class GoldCustomer extends Customer {
+
+    private double discountRatio;
+
+    @Override
+    public int calculatePrice(int price) {
+        this.bonusPoint += price * bonusPointRatio;
+        price -= price * discountRatio;
+        return price;
+    }
+
+    public GoldCustomer(String name){
+        super();
+        this.customerID = "Customer" + Customer.serialNums++;
+        this.name = name;
+        this.customerGrade = "GOLD";
+        this.bonusPoint = 0;
+        this.discountRatio = 0.03;
+        this.bonusPointRatio = 0.03;
+
+    }
+}
